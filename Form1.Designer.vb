417e2d67abc2721cdc8f1a43338d1f4d3f14e3ea
IchHabeKeinNamen <36535895+Banyc@users.noreply.github.com>
2018-05-30 20:45:46 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(647, 124)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(162, 165)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Font = New System.Drawing.Font("Georgia", 28.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(166, 199)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(493, 68)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Georgia", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(156, 270)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(168, 56)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 474)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TrackBar1.Location = New System.Drawing.Point(15, 346)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(644, 56)
+        Me.TrackBar1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(671, 515)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TrackBar1 As TrackBar
+End Class
---
+++ Form1.vb
@@ -0,0 +1,201 @@
+﻿Imports System.IO
+Public Class Form1
+    Public unit As Integer 'From 0
+    Public whichline As Integer
+    Public wordList() As String
+    Public english() As String
+    Public chinese() As String
+    Public mark() As Integer
+    Public whichLineInUnit As String
+    'Public mistakesIndex() As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "单词复习器 v0.1"
+        Label3.Text = "made by banic ，感谢zjs提供的单词表" & vbCrLf & "All Rights Reserved"
+        Label1.Text = ""
+        Label2.Text = ""
+        Dim f1 As StreamWriter = New StreamWriter("mistakes.txt", True, System.Text.Encoding.Default)
+        f1.Close()
+        Dim txt As String = ReadTxt("WordList.txt")
+        AddVbCrLfAtTheEndIfWithout(txt)
+        wordList = OrderIntoList(txt)
+        GetEnglishAndChineseFromArray(wordList)
+        Dim mark() As Integer = DivideIntoUnites(wordList)
+        For i = 0 To mark.LongCount - 1
+            ListBox1.Items.Add("第 " & i + 1 & " 单元")
+        Next
+    End Sub
+    Public Sub AddVbCrLfAtTheEndIfWithout(ByRef txt As String)
+        If Mid(txt, Len(txt) - 1) <> vbCrLf Then txt = txt & vbCrLf
+    End Sub
+    Public Function ReadTxt(ByVal address As String)
+        Dim reader As String
+        reader = File.ReadAllText(address, System.Text.Encoding.Default)
+        Return reader
+    End Function
+    Public Function OrderIntoList(ByVal txt As String)
+        Dim strArray() As String
+        strArray = Split(txt, vbCrLf)
+        Return strArray
+    End Function
+
+    Public Sub GetEnglishAndChineseFromArray(ByVal wordList() As String)
+        'Dim englishValue() As String
+        Dim englishStrs As String = ""
+        For i = 0 To wordList.LongCount - 1
+            ReDim Preserve english(i)
+            Dim strs As String = wordList(i)
+            'englishValue(i) = WordListValue(i)
+            For ii = 1 To Len(strs)
+                Dim str As String = ""
+                str = Mid(strs, ii, 1)
+                If Asc(str) < 0 Then
+                    Exit For
+                Else
+                    englishStrs = englishStrs & str
+                End If
+            Next
+            If englishStrs <> "" Then
+                english(i) = Mid(englishStrs, 1, Len(englishStrs) - 1)
+                englishStrs = ""
+            End If
+        Next
+
+        Dim chineseStrs As String = ""
+        For i = 0 To wordList.LongCount - 1
+            ReDim Preserve chinese(i)
+            Dim strs As String = wordList(i)
+            'englishValue(i) = WordListValue(i)
+            If strs <> "" Then
+                For ii = 1 To Len(strs)
+                    Dim str As String = ""
+                    str = Mid(strs, ii, 1)
+                    If Asc(str) < 0 Then
+                        chineseStrs = Mid(strs, ii)
+                        Exit For
+                    Else
+
+                    End If
+                Next
+                chinese(i) = chineseStrs
+                chineseStrs = ""
+            End If
+        Next
+    End Sub
+    Public Function DivideIntoUnites(ByVal strArray() As String)
+        Dim ii As Int16 = 0
+        For i = 0 To strArray.LongCount - 1
+            If strArray(i) = "" Then
+                ReDim Preserve mark(ii)
+                mark(ii) = i
+                ii = ii + 1
+            End If
+        Next
+        Return mark
+    End Function
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        unit = ListBox1.SelectedIndex
+        If unit <> ListBox1.Items.Count - 1 Then
+            whichline = mark(unit) + 1
+            Label1.Text = GetWord(whichline)
+            TextBox1.Text = ""
+            Label2.Text = ""
+            RefreshTrackbar1()
+        Else
+            ListBox1.SelectedIndex = unit - 1
+        End If
+    End Sub
+    Public Function GetWord(ByVal whichline As Integer)
+        Dim word As String
+        word = chinese(whichline)
+        Return word
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim text As String = TextBox1.Text
+        If IsThereAVbCrLf(text) Then
+            If IsCorrected(text, whichline) Then
+                whichline = whichline + 1
+                If Not IsFinished(whichline) Then
+                    Label1.Text = GetWord(whichline)
+                    TrackBar1.Value = GetWhichLineInUnit(whichline) - 1
+                Else
+
+                    unit = unit + 1
+                    If unit <> ListBox1.Items.Count - 1 Then
+                        'Label1.Text = "The End. 按回车继续下一单元"
+                        'RefreshTrackbar1()
+                        ListBox1.SelectedIndex = unit
+                    Else
+                        Label1.Text = "The End"
+                        unit = unit - 1
+                    End If
+                End If
+                    TextBox1.Text = ""
+                Label2.Text = ""
+
+            Else
+                If text <> "" Then
+                    TextBox1.Text = Mid(text, 1, Len(text) - 1)
+                Else
+                    TextBox1.Text = ""
+                End If
+                TextBox1.SelectAll()
+                Label2.Text = english(whichline)
+                'RecordMistakes(whichline)
+                WriteMistakes(whichline)
+            End If
+        End If
+
+    End Sub
+    Public Function IsThereAVbCrLf(ByVal text As String) As Boolean
+        If InStr(text, vbCrLf) <> 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Public Function IsCorrected(ByVal word As String, ByVal i As Int32) As Boolean
+        If word = english(i) & vbCrLf Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Public Function IsFinished(ByVal whichline As Integer) As Boolean
+        Dim a As Boolean = False
+        For i = 0 To mark.LongCount - 1
+            If mark(i) = whichline Then
+                a = True
+            End If
+        Next
+        Return a
+    End Function
+    'Public Sub RecordMistakes(ByVal whichline As Integer)
+    '    'Dim index As Integer
+    '    'index = mistakesIndex.LongCount - 1
+    '    'ReDim mistakesIndex(index + 1)
+    '    'mistakesIndex(index + 1) = whichline
+    'End Sub
+    Public Sub WriteMistakes(ByVal whichline As Integer)
+        Dim f1 As StreamWriter = New StreamWriter("mistakes.txt", True, System.Text.Encoding.Default)
+        f1.Write(wordList(whichline) & vbCrLf)
+        f1.Close()
+    End Sub
+    Public Function GetWhichLineInUnit(ByVal whichline As Integer)
+        whichLineInUnit = whichline - mark(unit)
+        Return whichLineInUnit
+    End Function
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        whichline = TrackBar1.Value + 1 + mark(unit)
+        Label1.Text = GetWord(whichline)
+        Label2.Text = ""
+    End Sub
+
+    Public Sub RefreshTrackbar1()
+        TrackBar1.Value = 0
+        TrackBar1.Maximum = mark(unit + 1) - mark(unit) - 2
+    End Sub
+End Class
